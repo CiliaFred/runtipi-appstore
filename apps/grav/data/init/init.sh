1dc502ad002ed5327b058b2fad9ae8ce8ad03751
Frédéric Cilia <AZR_FCILIA-ext@admin.lvmhpcis.com>
2024-02-28 00:06:18 +0100

    fix: Add an idempotent script for data location upgrade

--- apps/grav/data/init/init.sh
+++ apps/grav/data/init/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [ -d "/tmp/data/grav-comfig" ] && [ ! -d "/tmp/data/grav-config" ]; then
+if [ -d "/tmp/data/grav-comfig" ] -a [ ! -d "/tmp/data/grav-config" ]; then
   mv /tmp/data/grav-comfig /tmp/data/grav-config
 fi
